0edd29cbdd20e073842bbb875d12c123c622f2ae
jsu800 <jsu800@gmail.com>
2022-04-03 12:28:19 -0700

    adding business card without mode changers

---
+++ dark-mode-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BusinessCard from "./BusinessCard.js"
+
+function App() {
+  return (
+    <>
+      <BusinessCard />
+    </>
+  );
+}
+
+export default App;
---
+++ dark-mode-demo/src/BusinessCard.js
@@ -0,0 +1,26 @@
+function Name(props) {
+    return <h1>{props.name}</h1>
+}
+
+function Title(props) {
+    return <p>{props.title}</p>
+}
+
+function Email(props) {
+    return <p>{props.email}</p>
+}
+
+function Phone(props) {
+    return <p>{props.phone}</p>
+}
+
+export default function BusinessCard() {
+  return (
+    <div>
+      <Name name="Joe"/>
+      <Title title="Problem Solver"/>
+      <Email email="joe@mail.com"/>
+      <Phone phone="800-123-1234"/>
+    </div>
+  );
+}
