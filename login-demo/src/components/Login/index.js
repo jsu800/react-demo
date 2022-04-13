20cd978a393bb29b426feb5af963946817a284dd
jsu800 <jsu800@gmail.com>
2022-04-13 13:54:47 -0700

    adding react form http communications

--- login-demo/src/components/Login/index.js
+++ login-demo/src/components/Login/index.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LoginPage( {setToken}) {
+async function fetchToken(creds) {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    header: {
+      "Content-Type": "applicatioin/json",
+    },
+    body: JSON.stringify(creds),
+  }).then((data) => data.json());
+}
+
+function LoginPage({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const token = await fetchToken({
+      username,
+      password
+    });
+    setToken(token);
+  };
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(evt) => setUserName(evt.target.value)}
+          />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(evt) => setPassword(evt.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
