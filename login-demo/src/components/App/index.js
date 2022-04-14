ab5c852eddcbafd512725777231826e3b091726d
jsu800 <jsu800@gmail.com>
2022-04-13 17:02:33 -0700

    adding session storage code

--- login-demo/src/components/App/index.js
+++ login-demo/src/components/App/index.js
@@ -1,13 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import LandingPage from "../Landing";
 import LoginPage from "../Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavigationPage from "../Navigation";
 import * as ROUTES from "../../constants/paths";
 
+function setToken(userToken) {
+  sessionStorage.setItem("token", JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenItem = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenItem);
+  return userToken?.token;
+}
+
 function App() {
-  const [token, setToken] = useState();
-  
+  const token = getToken();
+
+  if (!token) {
+    return <LoginPage setToken={setToken} />;
+  }
+
   return (
     <>
       <h1>App</h1>
